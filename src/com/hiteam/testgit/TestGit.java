6d2209205b65e7fcada8ad4d37bd4ceeae04e4ff
codeChengWenY <1042732167@qq.com>
2016-09-18 14:48:29 +0800

    第三次提交

--- src/com/hiteam/testgit/TestGit.java
+++ src/com/hiteam/testgit/TestGit.java
@@ -3,7 +3,7 @@ package com.hiteam.testgit;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("say again Hello World");
+		System.out.println("say again Hello World");  
 	}
 
 }
